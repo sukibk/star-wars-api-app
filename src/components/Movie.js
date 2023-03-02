42fe43f221ede9995a607acd5d9767699297f56c
Marko Sudar <mjsudar21@catawba.edu>
2023-03-02 01:09:31 -0500

    Connecting with API

---
+++ src/components/Movie.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import classes from './Movie.module.css';
+
+const Movie = (props) => {
+  return (
+    <li className={classes.movie}>
+      <h2>{props.title}</h2>
+      <h3>{props.releaseDate}</h3>
+      <p>{props.openingText}</p>
+        {console.log(props.title, props.releaseDate, props.openingText)}
+    </li>
+  );
+};
+
+export default Movie;
